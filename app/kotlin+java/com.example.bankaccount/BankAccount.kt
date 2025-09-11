2268d5fa1da4c5fea9d66ea6f612b73b8ec7ed49
Nowshin Sayara <83832789+NSR123456@users.noreply.github.com>
2025-09-11 11:11:53 +0600

    Update BankAccount.kt

--- app/kotlin+java/com.example.bankaccount/BankAccount.kt
+++ app/kotlin+java/com.example.bankaccount/BankAccount.kt
@@ -1,1 +1,36 @@
+package com.example.bankaccount
 
+import androidx.collection.mutableIntSetOf
+
+class BankAccount (var name: String, var balance: Double){
+   var history = mutableListOf<String>()
+
+    fun deposit(amount:Double){
+        balance+=amount
+        history.add("Deposited $amount --Balance: $balance")
+    }
+
+    fun withdraw(amount: Double){
+        if(amount > balance){
+            println("Insufficient balance!!")
+        }
+        else{
+
+            balance -= amount
+            history.add("Withdrawn $amount -- Balance: $balance")
+        }
+    }
+    fun checkBalance(){
+        println("Balance: $balance")
+    }
+
+    fun checkHistory(){
+        if(history.isEmpty()){
+            println("No transaction yet!")
+        }
+        else{
+            history.forEach{ println(it)}
+        }
+    }
+
+}
