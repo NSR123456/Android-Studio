a2c774118eb6e413d09279e42ac5cf05f95f0c73
Nowshin Sayara <83832789+NSR123456@users.noreply.github.com>
2025-09-11 11:12:20 +0600

    Update main.kt

--- app/kotlin+java/com.example.bankaccount/main.kt
+++ app/kotlin+java/com.example.bankaccount/main.kt
@@ -1,1 +1,43 @@
+package com.example.bankaccount
 
+fun main(){
+    var acc = BankAccount( name ="John Doe", 1000.0)
+    while(true){
+        println("=========Bank Acoount Menu==============")
+        println("1. Deposit")
+        println("2. Withdraw")
+        println("3. Balance Check")
+        println("4. Transaction History")
+        println("5. Exit")
+
+        print("Enter your choice: ")
+        var ch = readln()?.toInt()
+
+        when(ch){
+            1 ->{
+                print("Enter amount to deposit: ")
+                var dep = readln().toDouble()
+                acc.deposit(dep)
+            }
+            2 ->{
+                print("Enter amount to withdraw: ")
+                var wit = readln().toDouble()
+                acc.withdraw(wit)
+            }
+            3 -> {
+                acc.checkBalance()
+            }
+            4 -> {
+                acc.checkHistory()
+            }
+            5 -> {
+                println("Exiting the program")
+                break
+            }
+            else -> {
+                println("Invalid choice, please try again!")
+            }
+        }
+    }
+
+}
